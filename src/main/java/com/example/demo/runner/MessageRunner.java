905401dd6b3c05217ab40ea926e730831bd5c42e
chinnammalu <chinnammalupeddinti@gmail.com>
2023-02-28 10:52:44 +0530

    wh-1-first time commit

---
+++ src/main/java/com/example/demo/runner/MessageRunner.java
@@ -0,0 +1,13 @@
+package com.example.demo.runner;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class MessageRunner implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Welcome to spring  boot app");
+		
+	}
+
+}
